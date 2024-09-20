ad9457fcd8f6a71dd2eddb82e7a93dce1f8ffa1b
Lawzin19 <168302047+Lawzin19@users.noreply.github.com>
2024-09-19 21:23:47 -0300

    Add files via upload

--- Porkus Follow/Porkus/src/main/java/br/com/law/porkus/Porkus.java
+++ Porkus Follow/Porkus/src/main/java/br/com/law/porkus/Porkus.java
@@ -1,40 +1,35 @@
 package br.com.law.porkus;
 
-
+import br.com.law.porkus.commands.InventuryCommand;
 import br.com.law.porkus.commands.SosCommand;
+import br.com.law.porkus.listeners.ChatListener;
 import br.com.law.porkus.listeners.GoldenBrickListener;
-import br.com.law.porkus.listeners.ItemPickupListener;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Piglin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public final class Porkus extends JavaPlugin {
 
+    private final HashMap<UUID, Piglin> playerPiglins = new HashMap<>();
+
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage("PorkuProjectEnable");
-        events();
-        commands();
+        Bukkit.getConsoleSender().sendMessage("Porkus está ativado!");
 
+        // Registrar eventos
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
+        getServer().getPluginManager().registerEvents(new GoldenBrickListener(this, playerPiglins), this);
+
+        // Registrar comandos
+        getCommand("bal").setExecutor(new InventuryCommand());
+        getCommand("sos").setExecutor(new SosCommand(this, playerPiglins));
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage("PorkuProjectDisable");
-    }
-
-    public void events() {
-        getServer().getPluginManager().registerEvents(new GoldenBrickListener(this), this);
-        getServer().getPluginManager().registerEvents(new ItemPickupListener(), this);
-    }
-
-    public void commands() {
-        CommandExecutor sosCommandExecutor = new SosCommand(this);
-
-        if (this.getCommand("sos") != null) {
-            this.getCommand("sos").setExecutor(sosCommandExecutor);
-        } else {
-            getLogger().severe("Comando 'sos' não encontrado no plugin.yml. Verifique se está registrado corretamente.");
-        }
+        Bukkit.getConsoleSender().sendMessage("Porkus está desativado!");
     }
 }
